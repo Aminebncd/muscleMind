528cbaf0e88b95410772344891afd19d32a6d3b9
Aminebncd <amine.bounachada@hotmail.com>
2024-04-04 23:02:03 +0200

    added controllers, set default route

---
+++ src/Controller/MuscleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MuscleController extends AbstractController
+{
+    #[Route('/muscle', name: 'app_muscle')]
+    public function index(): Response
+    {
+        return $this->render('muscle/index.html.twig', [
+            'controller_name' => 'MuscleController',
+        ]);
+    }
+}
