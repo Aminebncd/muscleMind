2d44fc7ccc400df1a8dcb2a511803161fa8cba20
Aminebncd <amine.bounachada@hotmail.com>
2024-05-02 23:56:18 +0200

    making some adjustments

--- src/Controller/MuscleController.php
+++ src/Controller/MuscleController.php
@@ -9,11 +9,45 @@ use Symfony\Component\Routing\Attribute\Route;
 class MuscleController extends AbstractController
 {
     #[Route('/muscle', name: 'app_muscle')]
-    
     public function index(): Response
     {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('muscle/index.html.twig', [
             'controller_name' => 'MuscleController',
         ]);
+    }
+
+    #[Route('/muscle/detailsMuscleGroup/{id}', name: 'app_muscleGroup_details')]
+    public function detailsMuscleGroup(Request $request,
+                                        MuscleRepository $mr): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('muscle/detailsMuscleGroup.html.twig', [
+            'controller_name' => 'MuscleController',
+            'MuscleGroup' => $muscleGroup,
+        ]);
+        // Blank for now
+    }
+
+
+    #[Route('/muscle/detailsMuscle/{id}', name: 'app_muscle_details')]
+    public function detailsMuscle(Request $request,
+                                MuscleRepository $mr): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('muscle/detailsMuscle.html.twig', [
+            'controller_name' => 'MuscleController',
+            'Muscle' => $muscle
+        ]);
+        
     }
 }
