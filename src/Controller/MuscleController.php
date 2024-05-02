614e236f07dce290e4ccdb332944e83d6c83fc6e
Aminebncd <amine.bounachada@hotmail.com>
2024-05-03 01:07:16 +0200

    working on getting muscleGroup infos when clicking on the svg

--- src/Controller/MuscleController.php
+++ src/Controller/MuscleController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\MuscleGroup;
+use App\Repository\MuscleRepository;
+use App\Repository\MuscleGroupRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MuscleController extends AbstractController
 {
@@ -22,32 +26,35 @@ class MuscleController extends AbstractController
 
     #[Route('/muscle/detailsMuscleGroup/{id}', name: 'app_muscleGroup_details')]
     public function detailsMuscleGroup(Request $request,
-                                        MuscleRepository $mr): Response
+                                        MuscleRepository $mr,
+                                        MuscleGroupRepository $mgr,
+                                        MuscleGroup $id): Response
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
+
+        dd($id);
 
         return $this->render('muscle/detailsMuscleGroup.html.twig', [
             'controller_name' => 'MuscleController',
             'MuscleGroup' => $muscleGroup,
         ]);
-        // Blank for now
     }
 
 
-    #[Route('/muscle/detailsMuscle/{id}', name: 'app_muscle_details')]
-    public function detailsMuscle(Request $request,
-                                MuscleRepository $mr): Response
-    {
-        if (!$this->getUser()) {
-            return $this->redirectToRoute('app_login');
-        }
+    // #[Route('/muscle/detailsMuscle/{id}', name: 'app_muscle_details')]
+    // public function detailsMuscle(Request $request,
+    //                             MuscleRepository $mr): Response
+    // {
+    //     if (!$this->getUser()) {
+    //         return $this->redirectToRoute('app_login');
+    //     }
 
-        return $this->render('muscle/detailsMuscle.html.twig', [
-            'controller_name' => 'MuscleController',
-            'Muscle' => $muscle
-        ]);
+    //     return $this->render('muscle/detailsMuscle.html.twig', [
+    //         'controller_name' => 'MuscleController',
+    //         'Muscle' => $muscle
+    //     ]);
         
-    }
+    // }
 }
