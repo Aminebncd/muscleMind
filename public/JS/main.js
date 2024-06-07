96421781f7033e6927434009c610a42b9e68328e
Aminebncd <amine.bounachada@hotmail.com>
2024-06-07 14:36:56 +0200

    fixed issue in JS

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,28 +1,47 @@
 
-console.log("JS file loaded");
+// just to ensure that the dom is fully loaded before running the script
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log("ici");
 
-// Simple JavaScript code to change the view of the card from front to back and vice versa
-document.getElementById("frontBtn").addEventListener("click", function() {
-    console.log("front button clicked");
-    document.getElementById("Frontview").style.display = "inline";
-    document.getElementById("Backview").style.display = "none";
-});
-    
-    document.getElementById("backBtn").addEventListener("click", function() {
-    console.log("back button clicked");
-    document.getElementById("Backview").style.display = "inline";
-    document.getElementById("Frontview").style.display = "none";
-});
+    // had an issue with the activity and progress buttons
+    // they were not working properly because the script was also looking
+    // for the front and back buttons which were not present in the activity page
+    // that led to the whole script being ignored
+    // so now I'm checking if the buttons are present before adding the event listeners
+    // works fine now
+    // i love javascript
+    let activityButton = document.getElementById('activityButton');
+    let progressButton = document.getElementById('progressButton');
+    let frontBtn = document.getElementById('frontBtn');
+    let backBtn = document.getElementById('backBtn');
 
-// same thing here but with the user trackings and activity
-document.getElementById("activityButton").addEventListener("click", function() {
-    console.log("activity button clicked");
-    // document.getElementById("ACTIVITY").style.display = "block";
-    // document.getElementById("PROGRESS").style.display = "none";
-});
+    if (activityButton && progressButton) {
+        activityButton.addEventListener('click', function() {
+            console.log("activity button clicked");
+            document.getElementById('ACTIVITY').style.display = "block";
+            document.getElementById('PROGRESS').style.display = "none";
+        });
 
-document.getElementById("progressButton").addEventListener("click", function() {
-    console.log("progress button clicked");
-    // document.getElementById("PROGRESS").style.display = "block";
-    // document.getElementById("ACTIVITY").style.display = "none";
+        progressButton.addEventListener('click', function() {
+            console.log("progress button clicked");
+            document.getElementById('PROGRESS').style.display = "block";
+            document.getElementById('ACTIVITY').style.display = "none";
+        });
+    }
+
+    if (frontBtn && backBtn) {
+        frontBtn.addEventListener("click", function() {
+            console.log("front button clicked");
+            document.getElementById("Frontview").style.display = "inline";
+            document.getElementById("Backview").style.display = "none";
+        });
+        
+        backBtn.addEventListener("click", function() {
+            console.log("back button clicked");
+            document.getElementById("Backview").style.display = "inline";
+            document.getElementById("Frontview").style.display = "none";
+        });
+    }
+
+    console.log("la");
 });
