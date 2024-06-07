4292d963ad650a95a86d1a45401ea8b861640634
Aminebncd <amine.bounachada@hotmail.com>
2024-06-07 12:04:26 +0200

    working on user profile

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,7 +1,9 @@
 
+console.log("JS file loaded");
 
 // Simple JavaScript code to change the view of the card from front to back and vice versa
 document.getElementById("frontBtn").addEventListener("click", function() {
+    // console.log("front button clicked");
     document.getElementById("Frontview").style.display = "inline";
     document.getElementById("Backview").style.display = "none";
 });
@@ -9,4 +11,17 @@ document.getElementById("frontBtn").addEventListener("click", function() {
 document.getElementById("backBtn").addEventListener("click", function() {
     document.getElementById("Backview").style.display = "inline";
     document.getElementById("Frontview").style.display = "none";
+});
+
+// same thing here but with the user trackings and activity
+document.getElementById("activityButton").addEventListener("click", function() {
+    console.log("activity button clicked");
+    document.getElementById("Frontview").style.display = "block";
+    document.getElementById("Backview").style.display = "none";
+});
+
+document.getElementById("progressButton").addEventListener("click", function() {
+    console.log("progress button clicked");
+    document.getElementById("Frontview").style.display = "block";
+    document.getElementById("Backview").style.display = "none";
 });
