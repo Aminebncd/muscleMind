8358c60475be3638be3f6889afb271572bc93a8a
Aminebncd <amine.bounachada@hotmail.com>
2024-06-21 14:33:32 +0200

    played around with plugins and different functions, added theme toggler

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { themeVariants } = require("tailwindcss-theme-variants");
+
 module.exports = {
   content: [
     "./assets/*.js",
@@ -8,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        nav:'#07080d', 
+        nav: '#07080d', 
         primary: '#000814',    
         secondary: '#001d3d',   
         tertiary: '#003566',   
@@ -16,7 +18,7 @@ module.exports = {
         quinary: '#00a6fb',    
         senary: '#ffc300', 
 
-        nav_light:'#0077b6', 
+        nav_light: '#0077b6', 
         primary_light: '#48cae4',    
         secondary_light: '#90e0ef',   
         tertiary_light: '#ade8f4',   
@@ -26,14 +28,16 @@ module.exports = {
       },
     },
   },
-  plugins: [themeVariants({
-    themes: {
-      light: {
-        mediaQuery: prefersLight /* "@media (prefers-color-scheme: light)" */,
-      },
-      dark: {
-        mediaQuery: prefersDark /* "@media (prefers-color-scheme: dark)" */,
-      },
-    },
-  }),],
+  plugins: [
+      themeVariants({
+        themes: {
+            light: {
+                selector: ".light-theme",
+            },
+            dark: {
+                selector: ".dark-theme",
+            },
+        },
+    }),
+  ],
 }
