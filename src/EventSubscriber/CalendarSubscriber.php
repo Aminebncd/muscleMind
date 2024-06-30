77d2c1db3e2b57ca81c5e6eee1bd52e11a74a959
Aminebncd <amine.bounachada@hotmail.com>
2024-06-30 23:52:39 +0200

    trying to debug calendar

--- src/EventSubscriber/CalendarSubscriber.php
+++ src/EventSubscriber/CalendarSubscriber.php
@@ -2,14 +2,25 @@
 
 namespace App\EventSubscriber;
 
-// use App\Entity\User;
 use CalendarBundle\Entity\Event;
 use CalendarBundle\Event\SetDataEvent;
-// use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class CalendarSubscriber implements EventSubscriberInterface
 {
+    private $tokenStorage;
+    private $authorizationChecker;
+    private $logger;
+
+    public function __construct(TokenStorageInterface $tokenStorage, AuthorizationCheckerInterface $authorizationChecker, LoggerInterface $logger)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->logger = $logger;
+    }
 
     public static function getSubscribedEvents()
     {
@@ -18,25 +29,45 @@ class CalendarSubscriber implements EventSubscriberInterface
         ];
     }
 
-
     public function onCalendarSetData(SetDataEvent $setDataEvent)
     {
+        $this->logger->info('onCalendarSetData called.');
 
-        $user = $this->getUser();
-
-        if (!$user) {
+        $token = $this->tokenStorage->getToken();
+        if (null === $token) {
+            $this->logger->warning('No token found.');
             return;
         }
 
+        $user = $token->getUser();
+        if (!is_object($user) || !$this->authorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            $this->logger->warning('User is not authenticated or not an object.');
+            return;
+        }
+
+        $this->logger->info('Authenticated user found.', ['user' => $user->getUsername()]);
+
         $sessions = $user->getSessions();
+        if ($sessions->isEmpty()) {
+            $this->logger->info('No sessions found for the user.');
+        } else {
+            $this->logger->info(sprintf('Found %d sessions for the user.', count($sessions)));
+        }
 
         foreach ($sessions as $session) {
+            $this->logger->info('Processing session.', [
+                'program' => $session->getProgram(),
+                'date' => $session->getDateSession()->format('Y-m-d H:i:s')
+            ]);
+
             $event = new Event(
-                (string)$session->getProgram(),
+                (string) $session->getProgram(),
                 $session->getDateSession()
             );
 
             $setDataEvent->addEvent($event);
+            $this->logger->info('Event added to the calendar.', ['event' => $event]);
         }
     }
 }
+
